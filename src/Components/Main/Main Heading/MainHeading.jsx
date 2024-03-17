9e42661a81514800d412c23ce20168cfbf1d7b95
sakibpro2003 <sakibprodhan2003@gmail.com>
2024-03-17 23:33:35 +0600

    first commit

---
+++ src/Components/Main/Main Heading/MainHeading.jsx
@@ -0,0 +1,10 @@
+const MainHeading = () => {
+    return (
+        <div className="text-center flex flex-col gap-[12px] lg:gap-[24px] lg:mt-[100px] mt-[30px] lg:mb-[50px] mb-[20px]">
+            <h1 className="lg:text-4xl text-2xl font-black">Our Recipes</h1>
+            <p className="w-[350px] lg:w-[600px] m-auto">Our recipe list is a meticulously curated compilation of culinary delights, carefully selected to inspire and satisfy any palate. From classic comfort foods to exotic international dishes, each recipe is a testament to the artistry of cooking and the joy of sharing meals with loved ones.</p>
+        </div>
+    );
+};
+
+export default MainHeading;
